296792aea18c44f17a4df126ef7ace0c90fd17a3
Arif S <70960883+rifset@users.noreply.github.com>
2024-01-04 21:33:30 +0700

    Create build_vocab.py

---
+++ build_vocab.py
@@ -0,0 +1,10 @@
+def build_vocab(texts):
+    sentences = texts.apply(lambda x: x.split()).values
+    vocab = {}
+    for sentence in sentences:
+        for word in sentence:
+            try:
+                vocab[word] += 1
+            except KeyError:
+                vocab[word] = 1
+    return vocab
